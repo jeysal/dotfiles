3153dabec8a548847be11d177076051b71340a43
Tim Seckinger <seckinger.tim@gmail.com>
2016-04-30 00:28:51 +0200

    added powerline shell to install script

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ echo "Downloading vim monokai theme..."
 mkdir -p ~/.vim/colors
 curl https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim > ~/.vim/colors/monokai.vim
 echo "Done"
+
+echo "Installing powerline shell prompt..."
+cd powerline-shell
+./install.py
+cd ..
+echo "Done"
