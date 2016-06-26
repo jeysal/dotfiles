2e74575da9ec7d3cbfb2770a18e5fa6bf7e13d30
Tim Seckinger <seckinger.tim@gmail.com>
2016-06-26 20:29:49 +0200

    removed powerline-shell

--- install.sh
+++ install.sh
@@ -8,10 +8,3 @@ echo -n "Installing vim monokai theme..."
 mkdir -p ~/.vim/colors
 cp vim-monokai/colors/monokai.vim ~/.vim/colors
 echo "Done"
-
-echo "Installing powerline shell prompt..."
-cd powerline-shell
-./install.py
-cp powerline-shell.py ~
-cd ..
-echo "Done"
