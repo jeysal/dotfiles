1fd19544a17afdf47360d725829aae7f272b2052
Tim Seckinger <seckinger.tim@gmail.com>
2017-03-12 23:03:51 +0100

    add systemd services

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo -n "Copying files to home directory..."
+echo -n "Installing home dir files..."
 cp -rT home/ ~/
 echo "Done"
 
 echo -n "Installing vim monokai theme..."
 mkdir -p ~/.vim/colors
 cp vim-monokai/colors/monokai.vim ~/.vim/colors
+echo "Done"
+
+echo -n "Installing systemd files..."
+sudo cp -rT systemd/ /etc/systemd/
 echo "Done"
