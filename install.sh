ce6cf287d1517bb909345699c34528fd653328d7
Tim Seckinger <seckinger.tim@gmail.com>
2017-04-07 10:29:12 +0200

    cp with -p

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 echo -n "Installing home dir files..."
-cp -rT home/ ~/
+cp -rpT home/ ~/
 mkdir -p ~/.vim/backups ~/.vim/swaps ~/.vim/undo
 echo "Done"
 
 echo -n "Installing base16-shell..."
 mkdir -p ~/.config/base16-shell
-cp -rT base16-shell/ ~/.config/base16-shell/
+cp -rpT base16-shell/ ~/.config/base16-shell/
 echo "Done"
 
 echo -n "Installing vim-pathogen..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-cp vim-pathogen/autoload/pathogen.vim ~/.vim/autoload
+cp -p vim-pathogen/autoload/pathogen.vim ~/.vim/autoload
 echo "Done"
 
 echo -n "Installing vim-plug..."
-cp vim-plug/plug.vim ~/.vim/autoload
+cp -p vim-plug/plug.vim ~/.vim/autoload
 echo "Done"
 
 echo -n "Installing systemd files..."
-sudo cp -rT systemd/ /etc/systemd/
+sudo cp -rpT systemd/ /etc/systemd/
 echo "Done"
