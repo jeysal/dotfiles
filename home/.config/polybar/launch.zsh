3329937eb57c6bd62c06f8c4eafaefa28920cf11
Tim Seckinger <seckinger.tim@gmail.com>
2017-08-17 13:37:32 +0200

    dynamically detect polybar monitor

--- home/.config/polybar/launch.zsh
+++ home/.config/polybar/launch.zsh
@@ -6,6 +6,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# detect monitor
+xrandrOutput=$(xrandr -q | grep " connected ")
+for monitor in DVI-D-0 HDMI1 eDP1 HDMI-0 VGA1; do
+  echo $monitor;
+  if echo "$xrandrOutput" | grep "$monitor"; then
+    export POLYBAR_MONITOR=$monitor
+  fi
+done
+
 # Launch bar1 and bar2
 polybar top &
 
