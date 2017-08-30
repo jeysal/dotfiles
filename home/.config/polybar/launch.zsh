711d1e4f3a0a0195b616a28b8ff424aac630f05d
Tim Seckinger <seckinger.tim@gmail.com>
2017-08-30 12:44:23 +0200

    add VGA-1 monitor as polybar option

--- home/.config/polybar/launch.zsh
+++ home/.config/polybar/launch.zsh
@@ -8,7 +8,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # detect monitor
 xrandrOutput=$(xrandr -q | grep " connected ")
-for monitor in DVI-D-0 eDP1 VGA1 HDMI-0 HDMI1; do
+for monitor in DVI-D-0 eDP1 VGA-1 VGA1 HDMI-0 HDMI1; do
   echo $monitor;
   if echo "$xrandrOutput" | grep "$monitor"; then
     export POLYBAR_MONITOR=$monitor
