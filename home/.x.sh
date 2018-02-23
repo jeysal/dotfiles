516f32c6e9eb593fd0330120b077eb769354abe6
Tim Seckinger <seckinger.tim@gmail.com>
2018-02-23 19:06:44 +0100

    remove obsolete files/lines

--- home/.x.sh
+++ home/.x.sh
@@ -1,18 +1,10 @@
 xrandr --auto
-# try desktop nouveau
-xrandr --output HDMI-1 --left-of DP-1
-xrandr --output DVI-D-1 --right-of DP-1
 # try desktop nvidia
 xrandr --output HDMI-0 --left-of DP-1
 xrandr --output DVI-D-0 --right-of DP-1
 # try notebook nvidia
 xrandr --output DP-2 --right-of HDMI-1
 xrandr --output eDP-1 --left-of HDMI-1
-# try VM
-xrandr --output VGA-3 --auto --right-of VGA-2
-xrandr --output VGA-2 --auto --left-of VGA-3
-xrandr --output VGA-2 --auto --right-of VGA-1
-xrandr --output VGA-1 --auto --left-of VGA-2
 
 # keys
 setxkbmap -layout de -variant nodeadkeys
--- home/.zprofile
+++ home/.zprofile
@@ -1,1 +1,1 @@
-export PATH="$HOME/bin:$HOME/.local/bin:$HOME/.gem/ruby/2.4.0/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
