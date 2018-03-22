c9f953eacf418a4ab4c32dd144ec04670a962fac
Tim Seckinger <seckinger.tim@gmail.com>
2018-03-22 21:51:16 +0100

    use rsync in install script

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo -n "Installing home dir files..."
-cp -rpT home/ ~/
+rsync -a home/ ~/
 mkdir -p ~/.vim/backups ~/.vim/swaps ~/.vim/undo
 echo "Done"
 
 echo -n "Installing base16-shell..."
 mkdir -p ~/.config/base16-shell
-cp -rpT base16-shell/ ~/.config/base16-shell/
+rsync -a base16-shell/ ~/.config/base16-shell/
 echo "Done"
 
 echo -n "Installing vim-plug..."
 mkdir -p ~/.local/share/nvim/site/autoload
-cp -p vim-plug/plug.vim ~/.local/share/nvim/site/autoload/plug.vim
+rsync -a vim-plug/plug.vim ~/.local/share/nvim/site/autoload/plug.vim
 echo "Done"
 
 if [ "$1" != "-a" ]; then
@@ -20,5 +20,5 @@ if [ "$1" != "-a" ]; then
 fi
 
 echo -n "Installing systemd files..."
-sudo cp -rpT systemd/ /etc/systemd/
+sudo rsync -a systemd/ /etc/systemd/
 echo "Done"
