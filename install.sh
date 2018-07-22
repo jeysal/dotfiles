c61f493be9e291e998e12b091ad55b2567dc3d4b
Tim Seckinger <seckinger.tim@gmail.com>
2018-07-22 12:16:57 +0200

    detect dangling symlinks in install.sh

--- install.sh
+++ install.sh
@@ -14,10 +14,16 @@ mkdir -p ~/.local/share/nvim/site/autoload
 ln -fnsT $(realpath vim-plug/plug.vim) ~/.local/share/nvim/site/autoload/plug.vim
 echo "Done"
 
-if [ "$1" != "-a" ]; then
-  exit
+if [ "$1" == "-a" ]; then
+  echo -n "Installing systemd files..."
+  sudo cp -nprsT $(realpath systemd)/ /etc/systemd/
+  echo "Done"
 fi
 
-echo -n "Installing systemd files..."
-sudo cp -nprsT $(realpath systemd)/ /etc/systemd/
-echo "Done"
+DANGLING_SYMLINKS=`find ~ -maxdepth 4 -xtype l`
+if [ -n "$DANGLING_SYMLINKS" ]; then
+  echo "Dangling symlinks:"
+  echo $DANGLING_SYMLINKS
+  echo "To remove, use:"
+  echo "find ~ -maxdepth 4 -xtype l -delete"
+fi
