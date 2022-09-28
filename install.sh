19cbe2cc4f0c01c7fda887742c14c1bfd56ba8f4
Tim Seckinger <seckinger.tim@gmail.com>
2022-09-28 16:29:39 +0200

    update

--- install.sh
+++ install.sh
@@ -37,8 +37,8 @@ ln -fnsT $(realpath vim-plug/plug.vim) ~/.local/share/nvim/site/autoload/plug.vi
 echo "Done"
 
 echo -n "Setting up nvm/npm..."
-NODE_VERSION=18.8.0
-NPM_GLOBAL_PACKAGES=(chrome-remote-interface@0.28.1 lid-sleep-daemon@2.0.1 serve@11.3.0 typescript-language-server@0.4.0)
+NODE_VERSION=18.9.1
+NPM_GLOBAL_PACKAGES=(chrome-remote-interface@0.31.3 lid-sleep-daemon@2.0.1)
 zsh -i -c "nvm install $NODE_VERSION && nvm alias default $NODE_VERSION && npm i -g $NPM_GLOBAL_PACKAGES"
 echo "Done"
 
