451617f336f21c42041c720cb856a4c06c0f996a
Tim Seckinger <seckinger.tim@gmail.com>
2022-10-27 20:57:49 +0200

    polybar consider VNC

--- home/.config/polybar/launch.zsh
+++ home/.config/polybar/launch.zsh
@@ -8,7 +8,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # detect monitor
 xrandrOutput=$(xrandr -q | grep " connected ")
-for monitor in DP-1 DP-0 DVI-D-1 DVI-D-0 HDMI-1 HDMI-0 HDMI1 HDMI0 VGA-1 VGA1 eDP eDP-1 eDP1 eDP-1-1; do
+for monitor in VNC-0 DP-1 DP-0 DVI-D-1 DVI-D-0 HDMI-1 HDMI-0 HDMI1 HDMI0 VGA-1 VGA1 eDP eDP-1 eDP1 eDP-1-1; do
   echo $monitor;
   if echo "$xrandrOutput" | grep "$monitor"; then
     export POLYBAR_MONITOR=$monitor
