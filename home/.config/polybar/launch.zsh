0f29ee60b4844ec1946921b28c7752f5b38f443d
Tim Seckinger <seckinger.tim@gmail.com>
2022-10-28 13:46:40 +0200

    gpu fixes

--- home/.config/polybar/launch.zsh
+++ home/.config/polybar/launch.zsh
@@ -6,15 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# detect monitor
-xrandrOutput=$(xrandr -q | grep " connected ")
-for monitor in VNC-0 DP-1 DP-0 DVI-D-1 DVI-D-0 HDMI-1 HDMI-0 HDMI1 HDMI0 VGA-1 VGA1 eDP eDP-1 eDP1 eDP-1-1; do
-  echo $monitor;
-  if echo "$xrandrOutput" | grep "$monitor"; then
-    export POLYBAR_MONITOR=$monitor
-  fi
-done
-
 # Launch
 polybar top &disown
 
--- home/.x.sh
+++ home/.x.sh
@@ -7,21 +7,21 @@ xrandr --output VGA-1 --auto --left-of VGA-2
 xrandr --output HDMI-0 --left-of DP-1
 xrandr --output DVI-I-1 --right-of DP-1
 # try notebook
-xrandr --output HDMI-1 --right-of eDP-1
-xrandr --output DP-1 --right-of eDP-1
-xrandr --output DP-2 --right-of eDP-1
+xrandr --output HDMI-A-0 --right-of eDP
+xrandr --output HDMI-A-1 --right-of eDP
+xrandr --output DisplayPort-1 --right-of eDP
 
 # keys
 setxkbmap -layout gb -variant mac -option compose:caps
 numlockx on
 
-# asus touchpad
+# asus input devices
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Natural Scrolling Enabled" 1
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Tapping Enabled" 0
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Accel Speed" 0.5
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Middle Emulation Enabled" 1 # disable the annoying default middle button region
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Disable While Typing Enabled" 0
-xinput --map-to-output "ELAN9008:00 04F3:2C82" eDP-1
+xinput --map-to-output "ELAN9008:00 04F3:2C82" eDP
 
 # beep
 xset -b
