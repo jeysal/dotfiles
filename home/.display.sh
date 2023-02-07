e02ad21c35cbba1b56f997d51960d5d79c08159c
Tim Seckinger <seckinger.tim@gmail.com>
2023-02-07 15:14:51 +0100

    prevent weird flickering with thunderbolt dock

--- home/.display.sh
+++ home/.display.sh
@@ -2,16 +2,18 @@ set -x
 exec >~/.local/share/display.log 2>&1
 date
 
-xrandr --auto
 # try VirtualBox
+xrandr --output VGA-2 --auto
 xrandr --output VGA-1 --auto --left-of VGA-2
 # try desktop
-xrandr --output HDMI-0 --left-of DP-1
-xrandr --output DVI-I-1 --right-of DP-1
+xrandr --output DP-1 --auto
+xrandr --output HDMI-0 --auto --left-of DP-1
+xrandr --output DVI-I-1 --auto --right-of DP-1
 # try notebook
-xrandr --output HDMI-1 --left-of eDP-1
-xrandr --output DP-1 --right-of eDP-1
-xrandr --output DP-2 --right-of eDP-1
+xrandr --output eDP-1 --auto
+xrandr --output HDMI-1 --auto --left-of eDP-1
+xrandr --output DP-1 --auto -right-of eDP-1
+xrandr --output DP-2 --auto --right-of eDP-1
 
 # map input devices
 xinput --map-to-output "ELAN9008:00 04F3:2C82" eDP-1
