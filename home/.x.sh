6cad8415c389e98020785e9905be10d77c5e2327
Tim Seckinger <seckinger.tim@gmail.com>
2023-03-12 22:09:17 +0100

    reliably .display.sh

--- home/.x.sh
+++ home/.x.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
+~/.display.sh # autorandr also executes this after profile load, but it might not find a profile on portable systems
 autorandr -c
 
 # asus touchpad
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Natural Scrolling Enabled" 1
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Tapping Enabled" 0
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Accel Speed" 0.5
-xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Middle Emulation Enabled" 1 # disable the annoying default middle button region
+xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Middle Emulation Enabled" 1
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Disable While Typing Enabled" 0
 
 # beep
