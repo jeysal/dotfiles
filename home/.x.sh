830da9e5ea9aaf04a0fb951c7ad5e7d4d0325087
Tim Seckinger <seckinger.tim@gmail.com>
2023-04-01 17:28:50 +0200

    logitech all-in-one keyboard xinput props

--- home/.x.sh
+++ home/.x.sh
@@ -10,6 +10,10 @@ xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Accel Speed" 0.5
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Middle Emulation Enabled" 1
 xinput set-prop "ELAN1201:00 04F3:3098 Touchpad" "libinput Disable While Typing Enabled" 0
 
+# logitech wireless all-in-one keyboard
+xinput set-prop "pointer:Logitech K400 Plus" "libinput Accel Speed" 1.0
+xinput set-prop "pointer:Logitech K400 Plus" "libinput Natural Scrolling Enabled" 1
+
 # beep
 xset -b
 
