61bcd65b66a0249a0c5f9aaedd2796317f113e68
Tim Seckinger <seckinger.tim@gmail.com>
2025-01-19 21:52:46 +0100

    SSH_AUTH_SOCK gcr or forward

--- etc/profile.d/systemd-user-environment.sh
+++ etc/profile.d/systemd-user-environment.sh
@@ -1,3 +1,8 @@
 set -o allexport
 source <(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)
+if [ ! -S ~/.ssh/ssh_auth_sock ]; then
+  SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
+else
+  SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+fi
 set +o allexport
