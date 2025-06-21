4fc932977e60d6a4154c3742de47261512f8585f
Tim Seckinger <seckinger.tim@gmail.com>
2025-06-21 19:21:46 +0200

    don't warn about autostart symlinks - in case of non graphical systems

--- install.sh
+++ install.sh
@@ -90,7 +90,7 @@ if [[ "$1" == "-a" ]] && uname | grep >/dev/null Linux; then
 fi
 
 echo -n "Looking for dangling symlinks..."
-DANGLING_SYMLINKS_CMD='find ~ -maxdepth 4 -xtype l ! -path "*/.config/BraveSoftware/*" ! -path "*/.thunderbird/*" ! -path "*/.config/discord/*" ! -path "*/.config/Signal/*" ! -path "*/.mozilla/firefox/*" ! -path "*/.steampath"'
+DANGLING_SYMLINKS_CMD='find ~ -maxdepth 4 -xtype l ! -path "*/.config/BraveSoftware/*" ! -path "*/.thunderbird/*" ! -path "*/.config/discord/*" ! -path "*/.config/Signal/*" ! -path "*/.mozilla/firefox/*" ! -path "*/.steampath" ! -path "*/.config/autostart/*"'
 DANGLING_SYMLINKS=`eval $DANGLING_SYMLINKS_CMD`
 if [ -n "$DANGLING_SYMLINKS" ]; then
   echo "Dangling symlinks:"
